9cadeecfc7b24000e5cfc56d622a17484cc423b4
niharikapal2603-cmyk <niharikapal2603@gmal.com>
2026-07-01 14:52:00 +0530

    myproj

--- tests/salesinventory/skipauth.spec.js
+++
@@ -1,5 +0,0 @@
-import {test} from "@playwright/test"
-test('skp auth',async({page})=>{
-    await page.goto("http://49.249.29.4:8081/AppServer/Sales_And_Inventory_System/pages/index.php")
-   await page.waitForTimeout(20000)
-})
